4af4ad10e41eea4398ca837b9f00e279d68e4af3
Yaquez-oss <scardorada444@gmail.com>
2026-04-07 19:14:14 -0600

    feat(auth): add role management and first-login password change

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // 1. Agrega esta línea
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // 2. Agrega esta línea aquí
+    tailwindcss(),
   ],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
